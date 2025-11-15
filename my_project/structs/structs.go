b5bb89c5a17baa4e6af385f7bed85b896ad1aa98
ken <kennethwarui35@gmail.com>
2025-11-15 19:39:11 +0300

    first commit

---
+++ my_project/structs/structs.go
@@ -0,0 +1,15 @@
+
+// my go file
+// type Person struct {
+//     Name string
+//     Age int
+//     Email string
+//     Address Address
+// }
+
+// type Address struct {
+//     street string
+//     City string
+//     Country string
+// }
+
