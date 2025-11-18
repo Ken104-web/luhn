5a7198c986f36146843dfb7f77aaf198763c7660
ken <kennethwarui35@gmail.com>
2025-11-18 19:51:39 +0300

    learned and added testing

---
+++ luhn2/luhntest.go
@@ -0,0 +1,25 @@
+package luhn2
+
+
+import "testing"
+
+func TestFunc(t *testing.T){
+    validNum := []int{
+        68431579246,
+        3719928465537204,
+        8274503916824457,
+        9364578220135794,
+        5527801946381276,
+        8146792058431168,
+        43298567124039,
+    }
+    for _, i := range validNum{
+        if !Valid(i){
+        t.Error("%v you should have a valid", i)
+    }
+    if Validateluhn(i/10) != i%10{
+        t.Error("%v's bro check your number it should be %v",i,i%10, Validateluhn(i/10))
+    }
+
+}
+}
